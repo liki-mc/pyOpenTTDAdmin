aa53cc0a7de9e6f724c5bfca91239406e03a877a
liki-mc <liki.mc@outlook.com>
2026-01-12 14:18:51 +0100

    added bugfix for company economy packet, also claryfying the quarterly info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pyOpenTTDAdmin',
-    version = '1.0.3',
+    version = '1.0.4',
     packages = ['pyopenttdadmin', 'aiopyopenttdadmin'],
     install_requires = [],  # Add any dependencies here
     author = 'liki-mc',
