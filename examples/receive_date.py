c9f65f758c5f296f6268022b69332be8844e2331
liki-mc <liki.mc@outlook.com>
2026-06-12 14:38:08 +0200

    updated examples

--- examples/receive_date.py
+++ examples/receive_date.py
@@ -1,12 +1,18 @@
-from pyopenttdadmin import Admin, AdminUpdateType, AdminUpdateFrequency, openttdpacket
+from pyopenttdadmin import Admin, AdminUpdateType, AdminUpdateFrequency, openttdpacket as p, Auth
 
 # Set the IP address and port number for connection
 ip_address = "127.0.0.1"
 port_number = 3977
 
+# Setup authentification
+auth = Auth(
+    name = "pyOpenTTDAdmin", # This name shows up in the logs
+    version = "15.0",
+    password = "toor" # assuming the password is 'toor'
+)
+
 # Instantiate the Admin class and establish connection to the server
-admin = Admin(ip = ip_address, port = port_number)
-admin.login(name = "pyOpenTTDAdmin", password = "toor")
+admin = Admin(ip = ip_address, port = port_number, auth = auth)
 
 # Subscribe to receive date updates and set the frequency for updates
 admin.subscribe(AdminUpdateType.DATE, AdminUpdateFrequency.DAILY)
@@ -16,8 +22,8 @@ admin.subscribe(AdminUpdateType.DATE, AdminUpdateFrequency.DAILY)
 # admin.send_subscribe(AdminUpdateType.DATE, AdminUpdateFrequency.MONTHLY)
 
 # Print date packets
-@admin.add_handler(openttdpacket.DatePacket)
-def date_packet(admin: Admin, packet: openttdpacket.DatePacket):
+@admin.add_handler(p.DatePacket)
+def date_packet(admin: Admin, packet: p.DatePacket):
     print(packet)
 
 # Keep the connection open and print incoming date packets
--- examples/send_chat.py
+++ examples/send_chat.py
@@ -1,19 +1,23 @@
-from pyopenttdadmin import Admin
-
+from pyopenttdadmin import Admin, Auth
 
 # Set the IP address and port number for connection
 ip_address = "127.0.0.1"
 port_number = 3977
 
+# Setup authentification
+auth = Auth(
+    name = "pyOpenTTDAdmin", # This name shows up in the logs
+    version = "15.0",
+    password = "toor" # assuming the password is 'toor'
+)
 
 # Instantiate the Admin class and establish connection to the server
-admin = Admin(ip = ip_address, port = port_number)
-admin.login(name = "pyOpenTTDAdmin", password = "toor")
+admin = Admin(ip = ip_address, port = port_number, auth = auth)
 
 # Send a global message to all players
-admin.send_global(message="Hello! I am a Python-powered OpenTTD bot!")
+admin.send_global(message = "Hello! I am a Python-powered OpenTTD bot!")
 
 # Send a private message to player with ID 1
-admin.send_private(message="This is a private message sent to ID 1", id=1)
+admin.send_private(message = "This is a private message sent to ID 1", id = 1)
 
 
